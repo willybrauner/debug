ae04dc4ddc3057ba94fbee22b69163151709f2a9
Willy Brauner <mail@willybrauner.com>
2021-11-01 18:16:05 +0100

    Improve browser example

--- example/browser/index.ts
+++ example/browser/index.ts
@@ -1,6 +1,21 @@
-import { menu } from "./Menu"
-const log = require("../../dist")("front:index");
+const debug = require("../../dist")
 
-log("index log", {props: "foo"});
-log("test 2");
-menu()
+const log = debug("front:index");
+log("Welcome debug!");
+log("Project version", "1.0.0");
+
+const log2 = debug("front:menu");
+log2("Hello menu!")
+log2("Menu is open", true)
+
+const log3 = debug("front:header");
+log3("Hello header!")
+
+const log4 = debug("front:very-long-namespace-1");
+log4("Hello namespace!")
+log4(10)
+log4({ foo: "bar" })
+
+const configLog1 = debug("config:task-1")
+configLog1("hello config:* namespace logs!")
+configLog1("This on is print only if localstorage debug value is set as 'config:*' or 'config:task-1'")
